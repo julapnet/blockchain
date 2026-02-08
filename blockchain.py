9036a7c018db0b2f257b97d7a54b6f527f487094
Raja Julapalli <julapnet@gmail.com>
2026-02-08 15:41:19 -0500

    Added is_valid method

--- blockchain.py
+++ blockchain.py
@@ -104,8 +104,29 @@ def get_chain():
 
     return jsonify(response), 200
 
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if(is_valid == True):
+        response = {'message':'Blockchain is valid'}
+    else:
+        response= {'message':'Blockchain is NOT VALID!'}
+        
+    return jsonify(response), 200
+
+
 # Running the app
-app.run(host='0.0.0.0', port = '5000')
+app.run(host='0.0.0.0', port = '6000')
+
+
+
+
+
+
+
+
+
 
 
 
